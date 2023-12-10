2709372f526f5bfbdcd63400c9aab787c5022518
Afx31 <post4wjm@gmail.com>
2023-12-10 21:47:21 +1100

    Main bulk of work, now to test on device

--- client/index.js
+++ client/index.js
@@ -1,6 +1,15 @@
-var socket = io.connect('localhost:3000');
+// var socket = io.connect('localhost:3000'); // Linux
+var socket = new WebSocket('ws://localhost:3000'); // Windows
 
-socket.on('CANBusMessage', (data) => {
+// Windows
+socket.onopen = () => {
+  console.log('Connected to the server');
+};
+
+// socket.on('CANBusMessage', (data) => { // Linux
+socket.onmessage = (event) => { // Windows
+  var data = JSON.parse(event.data); // Windows
+
   var rpmBar = document.getElementById('rpmbar');
   var rpmNum = document.getElementById('rpmNum');
   var speed = document.getElementById('speed');
@@ -14,20 +23,20 @@ socket.on('CANBusMessage', (data) => {
   var lambdaRatio = document.getElementById('lambdaRatio');
   // var inj = document.getElementById('inj');
   // var ign = document.getElementById('ign');
-  
+
   // RPM progressive bar
   rpmBar.style.setProperty('max-width', '1920px', 'important'); //1582px
-  var rpmbarPercentage = (data.rpm / 8300) * 100; // = (currentRpm/redlineRpm) * 100
+  var rpmbarPercentage = (data.rpm / 9000) * 100; // = (currentRpm/redlineRpm) * 100
 
   // TPS progressive bar
   tpsBar.style.setProperty('max-height', '400px', 'important');
 
   if (data.tps === 65535)
-    data.tps = 0
+    data.tps = 0;
 
   // Assign data to UI controls
-  rpmBar.style.width = rpmbarPercentage + '%';
-  tpsBar.style.height = data.tps + '%';
+  rpmBar.style.width = `${rpmbarPercentage}%`;
+  tpsBar.style.height = `${data.tps}%`;
   rpmNum.textContent = data.rpm;
   speed.textContent = data.speed;
   gear.textContent = data.gear;
@@ -41,14 +50,21 @@ socket.on('CANBusMessage', (data) => {
   // ign.textContent = data.ign;
 
   // RPM Bar colouring
-  var percentString = rpmBar.style.width;
-  percentString = percentString.split('%')
-  var percentInt = parseInt(percentString)
-
+  var percentInt = parseInt(rpmBar.style.width);
   if (percentInt > 90)
     rpmBar.style.setProperty('background-color', 'red', 'important');
   else if (percentInt > 65)
     rpmBar.style.setProperty('background-color', 'yellow', 'important');
   else
     rpmBar.style.setProperty('background-color', 'green', 'important');
-});
+};
+
+// Windows
+socket.onclose = () => {
+  console.log('Connection closed');
+};
+
+// Windows
+socket.onerror = (error) => {
+  console.error(`WebSocket error: ${error}`);
+};
