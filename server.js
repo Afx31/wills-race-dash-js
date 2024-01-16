8f47bd1938f86582a32e995ddb64789f8ee13b55
Afx31 <post4wjm@gmail.com>
2024-01-16 21:45:15 +1100

    carPIDConfig variable initial setup

--- server.js
+++ server.js
@@ -6,6 +6,26 @@ var socketio = require('socket.io')(server);
 
 var channel = can.createRawChannel('vcan0', true);
 
+var carPIDConfig = {
+  honda: {
+    rpm: { ids: [660, 1632], offset: 0, size: 2 },
+    speed: { ids: [660, 1632], offset: 2, size: 2 },
+    gear: { ids: [660, 1632], offset: 4, size: 1 },
+    voltage: { ids: [660, 1632], offset: 5, size: 1 },
+    iat: { ids: [661, 1633], offset: 0, size: 2 },
+    ect: { ids: [661, 1633], offset: 2, size: 2 },
+    tps: { ids: [662, 1634], offset: 0, size: 2 },
+    map: { ids: [662, 1634], offset: 2, size: 2 },
+    inj: { ids: [663, 1635], offset: 0, size: 2 },
+    ign: { ids: [663, 1635], offset: 2, size: 2 },
+    lambdaRatio: { ids: [664, 1636], offset: 0, size: 2 },
+    lambda: { ids: [664, 1636], offset: 2, size: 2 }
+  },
+  mazda: {
+    tps: { ids: [201], offset: 5, size: 1 },
+  }
+}
+
 var canbusData = {
   rpm: 0,
   speed: 0,
@@ -30,6 +50,7 @@ socketio.on('connection', function(client) {
 setInterval(() => {
   socketio.emit('CANBusMessage', canbusData);
 }, 100);
+
 
 channel.addListener('onMessage', function(msg) {
   // Rpm, speed, gear, voltage
